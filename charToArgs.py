8c7c189dd485c8d63ef3c27aa8d9ec0437d2f665
lennonlkc <3116005182@mail2.gdut.edu.cn>
2018-09-09 21:51:26 +0800

    修正空指令的输入

--- charToArgs.py
+++ charToArgs.py
@@ -3,11 +3,16 @@ import sys
 
 from optparse import OptionParser
 parser = OptionParser()
-parser.add_option("-f", action='store', type='string', dest='filename')
+parser.add_option("-w", action='store', type='string', dest='words')
+parser.add_option("-l", action='store', type='string', dest='lines')
+parser.add_option("-c", action='store', type='string', dest='chars')
+#parser.add_option("-A", action='store', type='string', dest='filename')
 
 (values,args) = parser.parse_args()
-name = values.filename
-print("the file name is",name)
+#name = values.words
+#print("the file name is",name)
+options,args = parser.parse_args()
+
 def file_word(filename):
     file = open(filename, "r+")
     wordN = 1
@@ -34,6 +39,34 @@ def file_char(filename):
             charN = charN + 1
     print("The number of chars in this document is:",charN )
 
+#Cause some unexpected chars appear in the file name , so use the clear_filename to repair
+
+def clear_filename(string):
+    clearBox = "[]\'"
+    for i in clearBox:
+        string = string.replace(i,"")
+    return string
+
+
+if not (options.chars or options.words or options.lines):
+    name = str(args)
+    name = clear_filename(name)
+    file_line(name)
+    file_word(name)
+    file_char(name)
+
+if options.chars:
+    name = values.chars
+    file_char(name)
+if options.words:
+    name = values.words
+    file_word(name)
+if options.lines:
+    name = values.lines
+    file_line(name)
+
+"""
 file_line(name)
 file_word(name)
 file_char(name)
+"""
