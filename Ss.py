bf87b15ed4b5948e2ad23fef153bb3b3adabcc0f
lennonlkc <3116005182@mail2.gdut.edu.cn>
2018-09-09 21:53:24 +0800

    Ss是最新更新的主文件

--- Ss.py
+++ Ss.py
@@ -1,26 +1,72 @@
+import sys
 
-def file_word():
-    file = open("2.txt", "r+")
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-w", action='store', type='string', dest='words')
+parser.add_option("-l", action='store', type='string', dest='lines')
+parser.add_option("-c", action='store', type='string', dest='chars')
+#parser.add_option("-A", action='store', type='string', dest='filename')
+
+(values,args) = parser.parse_args()
+#name = values.words
+#print("the file name is",name)
+options,args = parser.parse_args()
+
+def file_word(filename):
+    file = open(filename, "r+")
     wordN = 1
     for str in file.read():
         if(str == ' '):
             wordN = wordN + 1
-    print("文档的单词数是:",wordN)
+    print("The number of words in this document is:",wordN )
 
-def file_line():
-    file = open("2.txt","r+")
+def file_line(filename):
+    file = open(filename,"r+")
     lineN = 1
     for i in file.read():
         if(i == '\n'):
             lineN = lineN + 1
-    print("文档的行数是:",lineN)
+    print("The number of lines in this document is:",lineN )
 
-def file_char():
-    file = open("2.txt","r+")
+def file_char(filename):
+    file = open(filename,"r+")
     charN = 0
     for stc in file.read():
         if(stc == '\n'):
             charN
         else:
             charN = charN + 1
-    print("文档的字符数是:", charN)
+    print("The number of chars in this document is:",charN )
+
+#Cause some unexpected chars appear in the file name , so use the clear_filename to repair
+
+def clear_filename(string):
+    clearBox = "[]\'"
+    for i in clearBox:
+        string = string.replace(i,"")
+    return string
+
+
+if not (options.chars or options.words or options.lines):
+    name = str(args)
+    name = clear_filename(name)
+    file_line(name)
+    file_word(name)
+    file_char(name)
+
+if options.chars:
+    name = values.chars
+    file_char(name)
+if options.words:
+    name = values.words
+    file_word(name)
+if options.lines:
+    name = values.lines
+    file_line(name)
+
+"""
+file_line(name)
+file_word(name)
+file_char(name)
+"""
